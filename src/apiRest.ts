7b5804cc27fd6e2b43a4ed4b7852b3d885a76011
Mario Dariel <mdariel663@gmail.com>
2024-10-25 04:46:32 -0400

    Agregando Users al path principal de la aplicacion y cambiando endpoints a /api/v2

--- src/apiRest.ts
+++ src/apiRest.ts
@@ -2,7 +2,10 @@ import dotenv from "dotenv";
 import express, { Request, Response , NextFunction} from "express";
 import cors from "cors"
 import MiddlewareErrorHandler from "./controllers/middleware/MiddlewareErrors";
+
+// routes
 import LogRouter from "./routes/logs";
+import UserRouter from "./routes/user";
 dotenv.config();
 
 const app = express();
@@ -16,7 +19,9 @@ app.use(express.json());
 app.use(cors())
 app.use(MiddlewareErrorHandler)
 // agregar rutas
-app.use("/log", LogRouter);
+app.use("/api/v2/log", LogRouter);
+app.use("/api/v2/user", UserRouter);
+
 try{
   app.listen(port, () => {
     console.log(`Servidor corriendo en puerto ${port}`);
