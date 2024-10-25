ed3d2a57a61599f2931da85b9e6cf25629c0369c
Mario Dariel <mdariel663@gmail.com>
2024-10-25 03:41:12 -0400

    Implementando Middleware de manejo de errores generales

--- src/apiRest.ts
+++ src/apiRest.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express, { Request, Response , NextFunction} from "express";
 import cors from "cors"
+import MiddlewareErrorHandler from "./controllers/middleware/MiddlewareErrors";
 dotenv.config();
 
 const app = express();
@@ -14,7 +15,7 @@ app.use(express.json());
 app.use(cors())
 app.use(MiddlewareErrorHandler)
 // agregar rutas
-
+try{
   app.listen(port, () => {
     console.log(`Servidor corriendo en puerto ${port}`);
   });
