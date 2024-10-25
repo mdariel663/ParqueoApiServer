fe77ebfaf76a58b5ae6dc03d482ee839a437d9ae
Mario Dariel <mdariel663@gmail.com>
2024-10-25 13:52:26 -0400

    refactorizando UserServices

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -8,68 +8,134 @@ import UserModelError from "../models/Errors/UserModelError";
 import PhoneRequest from "../models/User/PhoneRequest";
 
 class UserService {
-    static async deleteUser(currentUserId: UUID, userDeleteId: UUID) {
-        const userModel = new UserModel();
-        const currentUser = await userModel.getCurrentUser(currentUserId);
-        if (!currentUser || currentUser.role !== "admin") {
-            throw new UserModelError("No se puede eliminar un usuario si usted no es administrador");
+  static async deleteUser(currentUserId: UUID, userDeleteId: UUID | undefined) {
+    const userModel = new UserModel();
+    const currentUser = await userModel.getCurrentUser(currentUserId);
+
+    if (!userDeleteId) {
+      userDeleteId = currentUserId;
+    } else if (currentUser.role !== "admin") {
+      throw new UserModelError(
+        "No se puede eliminar un usuario si usted no es administrador."
+      );
+    }
+
+    const userData = await userModel.getUserById(userDeleteId);
+
+    if (!userData) throw new UserModelError("El usuario no existe");
+    await userModel.deleteUser(userDeleteId);
+    return {
+      success: true,
+      message: "Usuario eliminado",
+      userId: userDeleteId,
+    };
+  }
+
+  static async createUser({
+    name,
+    email,
+    phone,
+    password,
+    role,
+  }: {
+    name: string;
+    email: string;
+    phone: string;
+    password: string;
+    role: string;
+  }) {
+    const userModel = new UserModel();
+    const requestData = {
+      name: new UserNameRequest(name),
+      email: new EmailRequest(email),
+      phone: new PhoneRequest(phone),
+      password: new PasswordRequest(password),
+      role: new RoleRequest(role),
+    };
+
+    (Object.keys(requestData) as (keyof typeof requestData)[]).forEach(
+      (key) => {
+        if (requestData[key].messageError) {
+          throw new UserModelError(requestData[key].messageError);
         }
-        const userData = await userModel.getUserById(userDeleteId);
-        if (!userData) throw new UserModelError("El usuario no existe");
-        await userModel.deleteUser(userDeleteId);
-        return { success: true, message: "Usuario eliminado", userId: userDeleteId };
+      }
+    );
+
+    const user = await userModel.create(requestData);
+    return { success: true, user };
+  }
+
+  static async updateUser(
+    userId: UUID,
+    name?: string,
+    email?: string,
+    phone?: string,
+    password?: string,
+    role?: string
+  ) {
+    const userModel = new UserModel();
+    return await userModel.updateUser(userId, {
+      name,
+      email,
+      phone,
+      password,
+      role,
+    });
+  }
+
+  static async getCurrentUser(userId: UUID) {
+    const userModel = new UserModel();
+    const user = await userModel.getCurrentUser(userId);
+    if (!user) throw new UserModelError("Usuario actual no encontrado");
+    return user;
+  }
+
+  static async getUsers() {
+    const userModel = new UserModel();
+    return await userModel.getUsers();
+  }
+
+  static async loginUser(
+    email: string | undefined,
+    phone: string | undefined,
+    password: string
+  ) {
+    if (!password || (!email && !phone)) {
+      throw new UserModelError(
+        "Los campos email o phone y password son obligatorios"
+      );
     }
 
-    static async createUser({ name, email, phone, password, role }: { name: string, email: string,phone: string, password: string, role: string }) {
-        const userModel = new UserModel();
-        const requestData = {
-            name: new UserNameRequest(name),
-            email: new EmailRequest(email),
-            phone: new PhoneRequest(phone),
-            password: new PasswordRequest(password),
-            role: new RoleRequest(role),
-        };
+    const emailRequest = email ? new EmailRequest(email) : null;
+    const phoneRequest = phone ? new PhoneRequest(phone) : null;
+    const passwordRequest = new PasswordRequest(password);
 
-        (Object.keys(requestData) as (keyof typeof requestData)[]).forEach((key) => {
-            if (requestData[key].messageError) {
-                throw new UserModelError(requestData[key].messageError);
-            }
-        });
-
-        const user = await userModel.create(requestData);
-        return { success: true, user };
+    if (emailRequest && !emailRequest.isValid) {
+      throw new UserModelError("El formato de email no es válido");
     }
 
-    static async updateUser(userId: UUID, name?: string, email?: string, phone?: string, password?: string, role?: string) {
-        const userModel = new UserModel();
-        return await userModel.updateUser(userId, { name, email, phone, password, role });
+    if (phoneRequest && !phoneRequest.isValid) {
+      throw new UserModelError("El formato de phone no es válido");
     }
 
-    static async getCurrentUser(userId: UUID) {
-        const userModel = new UserModel();
-        const user = await userModel.getCurrentUser(userId);
-        if (!user) throw new UserModelError("Usuario actual no encontrado");
-        return user;
+    if (!passwordRequest.isValid) {
+      throw new UserModelError(passwordRequest.messageError);
     }
 
-    static async getUsers() {
-        const userModel = new UserModel();
-        return await userModel.getUsers();
+    const userModel = new UserModel();
+    const user = await userModel.checkLogin(
+      emailRequest || phoneRequest!,
+      passwordRequest
+    );
+
+    if (!user) {
+      throw new UserModelError(
+        "El usuario no existe o la contraseña es incorrecta"
+      );
     }
 
-    static async loginUser(email: string, password: string) {
-        const emailRequest = new EmailRequest(email);
-        const passwordRequest = new PasswordRequest(password);
-
-        if (!emailRequest.isValid || !passwordRequest.isValid) {
-            throw new UserModelError("Los campos email y password son obligatorios");
-        }
-        
-        const userModel = new UserModel();
-        const user = await userModel.checkLogin(emailRequest, passwordRequest);
-        if (!user) throw new UserModelError("El usuario no existe o la contraseña es incorrecta");
-        return { success: true, message: "Usuario autenticado", ...user };
-    }
+    return { success: true, message: "Usuario autenticado", ...user };
+  }
 }
 
 export default UserService;
