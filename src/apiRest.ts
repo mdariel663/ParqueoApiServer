076d2b37594c253c40cdb4d1831cdef77f6e5a74
Mario Dariel <mdariel663@gmail.com>
2024-10-25 04:33:25 -0400

    Implementando ruta de logs y agregandolo a ruta principal

--- src/apiRest.ts
+++ src/apiRest.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express, { Request, Response , NextFunction} from "express";
 import cors from "cors"
 import MiddlewareErrorHandler from "./controllers/middleware/MiddlewareErrors";
+import LogRouter from "./routes/logs";
 dotenv.config();
 
 const app = express();
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(cors())
 app.use(MiddlewareErrorHandler)
 // agregar rutas
+app.use("/log", LogRouter);
 try{
   app.listen(port, () => {
     console.log(`Servidor corriendo en puerto ${port}`);
---
+++ src/routes/logs.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import controllers from "../controllers/controllers";
+const router = express.Router();
+
+router.get("/", controllers.middleware.onlyAutenticated, controllers.middleware.authorizeAdmin, (req, res) => {
+
+  const action = req.query.action;
+  controllers.persistentLog
+    .getLogs(action?.toString())
+    .then((logs) => res.status(200).json(logs))
+    .catch((_) => {
+      res.status(500).json({ error: "Error al obtener los registros" });
+    });
+});
+
+export default router;
