6741840fe94a65acb44713c985025dd79d2ffe6d
Mario Dariel <mdariel663@gmail.com>
2024-10-26 01:18:53 -0400

    update setup db (.ts)

--- src/setupDB.ts
+++ src/setupDB.ts
@@ -4,7 +4,8 @@ import mysql from 'mysql2/promise'
 import dotenv from 'dotenv'
 
 dotenv.config()
-const {DBHOST_MYSQL_DEFAULT, DBUSER_MYSQL_DEFAULT, DBPASSWORD_MYSQL_DEFAULT} = process.env
+const { DBHOST_MYSQL_DEFAULT, DBUSER_MYSQL_DEFAULT, DBPASSWORD_MYSQL_DEFAULT } =
+  process.env
 
 const dbConfig = {
   host: DBHOST_MYSQL_DEFAULT,
@@ -13,50 +14,60 @@ const dbConfig = {
 }
 
 const setupDatabase = async () => {
-  try {
+  // try {
+  console.log('=============== Datos ingresados ===============')
+  console.log('Config: ', dbConfig)
+  // Crear conexión a la base de datos
+  await mysql
+    .createConnection(dbConfig)
+    .then(async (conn) => {
+      console.log('Conexión a la base de datos establecida')
 
-    console.log('=============== Datos ingresados ===============')
-    console.log('Config: ', dbConfig)
-    // Crear conexión a la base de datos
-    const connection = await mysql.createConnection(dbConfig)
-    console.log('Conexión a la base de datos establecida')
+      // Leer el archivo setupdb.sql
+      const sqlFilePath = path.join(__dirname, 'setupdb.sql')
+      const sql = fs.readFileSync(sqlFilePath, 'utf-8')
 
-    // Leer el archivo setupdb.sql
-    const sqlFilePath = path.join(__dirname, 'setupdb.sql')
-    const sql = fs.readFileSync(sqlFilePath, 'utf-8')
+      // Dividir el archivo en líneas de comandos SQL
+      const sqlStatements = sql
+        .split(';')
+        .map((statement) => statement.trim())
+        .filter((statement) => statement.length > 0)
 
-    // Dividir el archivo en líneas de comandos SQL
-    const sqlStatements = sql
-      .split(';')
-      .map(statement => statement.trim())
-      .filter(statement => statement.length > 0)
+      // Ejecutar cada línea de comando
+      for (const statement of sqlStatements) {
+        console.log(`Ejecutando: ${statement}`)
+        await conn.query(statement)
+      }
 
-    // Ejecutar cada línea de comando
-    for (const statement of sqlStatements) {
-      console.log(`Ejecutando: ${statement}`)
-      await connection.query(statement)
-    }
+      console.log('Base de datos configurada correctamente.')
+      await conn.end()
+    })
+    .catch((err) => {
+      const { code, message } = err
+      switch (code) {
+        case 'ER_ACCESS_DENIED_ERROR':
+          console.error(
+            '⚠️ Acceso denegado: Revisa tus credenciales de base de datos en el archivo .env. Puede que necesites permisos de root o verificar el nombre de usuario y contraseña.'
+          )
+          break
+        case 'ER_ACCESS_DENIED_NO_PASSWORD_ERROR':
+          console.error(
+            '⚠️ Acceso denegado: Parece que la contraseña no se proporcionó. Verifica que el campo DBPASSWORD_MYSQL_DEFAULT en el archivo .env no esté vacío.'
+          )
+          break
+        case 'ENOTFOUND':
+          console.error(
+            '⚠️ Error de conexión: No se pudo encontrar el host especificado. Revisa que DBHOST_MYSQL_DEFAULT esté correcto en el archivo .env.'
+          )
+          break
+        default:
+          console.error(`⚠️ Error configurando la base de datos: ${message}`)
+      }
+    })
 
-    console.log('Base de datos configurada correctamente.')
-    await connection.end()
-  } catch (error: any) {   
-    const { code, message } = error
+  // } catch (error) {
 
-    // Mensajes personalizados según el código de error de MySQL
-    switch (code) {
-      case 'ER_ACCESS_DENIED_ERROR':
-        console.error('⚠️ Acceso denegado: Revisa tus credenciales de base de datos en el archivo .env. Puede que necesites permisos de root o verificar el nombre de usuario y contraseña.');
-        break
-      case 'ER_ACCESS_DENIED_NO_PASSWORD_ERROR':
-        console.error('⚠️ Acceso denegado: Parece que la contraseña no se proporcionó. Verifica que el campo DBPASSWORD_MYSQL_DEFAULT en el archivo .env no esté vacío.');
-        break
-      case 'ENOTFOUND':
-        console.error('⚠️ Error de conexión: No se pudo encontrar el host especificado. Revisa que DBHOST_MYSQL_DEFAULT esté correcto en el archivo .env.');
-        break
-      default:
-        console.error(`⚠️ Error configurando la base de datos: ${message}`);
-    }
-  }
+  //  }
 }
 
 setupDatabase()
