ad4389a514e210bac79f8f3b7f1344cfe9fd6ba4
Mario Dariel <mdariel663@gmail.com>
2024-10-29 16:03:29 -0400

    refactoring logs routes

--- src/routes/logs.ts
+++ src/routes/logs.ts
@@ -1,16 +1,20 @@
-import express from "express";
-import controllers from "../controllers/controllers";
-const router = express.Router();
+import express from 'express'
+import controllers from '../controllers/controllers'
+const router = express.Router()
 
-router.get("/", controllers.middleware.onlyAutenticated, controllers.middleware.authorizeAdmin, (req, res) => {
+router.get(
+  '/',
+  controllers.middleware.onlyAuthenticated,
+  controllers.middleware.authorizeAdmin,
+  (req, res) => {
+    const action = req.query.action
+    controllers.persistentLog
+      .getLogs(action?.toString())
+      .then((logs) => res.status(200).json(logs))
+      .catch((_) => {
+        res.status(500).json({ error: 'Error al obtener los registros' })
+      })
+  }
+)
 
-  const action = req.query.action;
-  controllers.persistentLog
-    .getLogs(action?.toString())
-    .then((logs) => res.status(200).json(logs))
-    .catch((_) => {
-      res.status(500).json({ error: "Error al obtener los registros" });
-    });
-});
-
-export default router;
+export default router
