9f4e50d9f0937baee12152aba823314aa07052b1
Mario Dariel <mdariel663@gmail.com>
2024-10-29 16:03:10 -0400

    adding filter in userService

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -6,6 +6,7 @@ import UserNameRequest from "../models/User/UserNameRequest";
 import { UUID } from "crypto";
 import UserModelError from "../models/Errors/UserModelError";
 import PhoneRequest from "../models/User/PhoneRequest";
+import UserFilterModel from "../models/UserFilterModel";
 
 class UserService {
   static async deleteUser(currentUserId: UUID, userDeleteId: UUID | undefined) {
@@ -53,16 +54,29 @@ class UserService {
       role: new RoleRequest(role),
     };
 
-    (Object.keys(requestData) as (keyof typeof requestData)[]).forEach(
-      (key) => {
-        if (requestData[key].messageError) {
-          throw new UserModelError(requestData[key].messageError);
-        }
+    // Verificar si hay errores en los datos
+    (Object.keys(requestData) as (keyof typeof requestData)[]).forEach((key) => {
+      if (requestData[key].messageError) {
+        throw new UserModelError(requestData[key].messageError);
       }
-    );
+    });
 
+    const userExists = await UserService.userExists(email, phone);
+    if (userExists) {
+      throw new UserModelError('El correo electrónico o el teléfono ya están en uso.');
+    }
+    // Crear el usuario en la base de datos
     const user = await userModel.create(requestData);
     return { success: true, user };
+  }
+
+
+
+  static async userExists(email: string, phone: string): Promise<boolean> {
+    const userModel = new UserModel();
+    const filter = new UserFilterModel({ "email": email, "phone": phone });
+    const user = await userModel.findUserByFilter(email, phone, filter);
+    return user === null;
   }
 
   static async updateUser(
